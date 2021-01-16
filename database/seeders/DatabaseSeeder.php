77d0e06b5e7ebe1ca0793bd4fe7c543ead3c9cd4
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-01-16 13:27:52 +0700

    Configure seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(RoleSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(UsersSeeder::class);
+        
+        
+        
     }
 }
