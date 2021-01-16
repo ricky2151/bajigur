974bc126cb0e1924d7c07c309a3254eaeb4b80ce
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-01-16 13:20:40 +0700

    Make Route API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix'=>'auth'], function()
+{
+    Route::post('register', 'AuthController@registerAsUser');
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
 });
+
+Route::group(['prefix' => 'admin', 'middleware' => 'RoleAdmin', 'as'=>'admin.'], function()
+{
+    Route::get('users/history_transactions/{user}', 'UserController@historyTransaction');
+    Route::resource('users', 'UserController')->only([
+        'index','store', 'destroy', 'update'
+    ]);
+
+    Route::resource('categories', 'CategoryController')->except([
+        'create', 'show'
+    ]);
+    Route::resource('products', 'ProductController')->except([
+        'create', 'show'
+    ]);
+    Route::resource('transactions', 'TransactionController')->only([
+        'index', 'show'
+    ]);
+});
+
+Route::group(['prefix' => 'user', 'middleware' => 'RoleUser', 'as'=>'user.'], function()
+{
+    Route::patch('users/update_me', 'UserController@updateMe');
+    Route::get('transactions/my_history_transaction', 'TransactionController@myHistoryTransaction');
+
+    Route::resource('products', 'ProductController')->only([
+        'index'
+    ]);
+
+    Route::resource('transactions', 'TransactionController')->only([
+        'store'
+    ]);
+});
+
+
+Route::any('{any}', function(){
+    return response()->json([
+        'status'    => false,
+        'message'   => 'API Not Found.',
+    ], 404);
+})->where('any', '.*');
